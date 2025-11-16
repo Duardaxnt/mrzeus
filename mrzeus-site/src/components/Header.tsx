8453c631b7249f685c3922dc05010cd2f6220fc6
Duardaxnt <nathallyandrade99@gmail.com>
2025-11-15 21:53:10 -0300

    Header-finalizado

--- mrzeus-site/src/components/Header.tsx
+++ mrzeus-site/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="logo-area">
-                <img src="/logo.png" alt="Logo" className="logo" />
+                <img src="/logo.jpg" alt="Logo" className="logo" />
                 <h1>Mr.Zeus</h1>
             </div>
         </header>
