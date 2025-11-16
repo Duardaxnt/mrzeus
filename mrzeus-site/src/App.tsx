c1bb49914ff962fb7040a2222a4ea74ed17696ca
Duardaxnt <nathallyandrade99@gmail.com>
2025-11-15 21:13:53 -0300

    Primeiro commit

--- mrzeus-site/src/App.tsx
+++ mrzeus-site/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/logo.jpg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
